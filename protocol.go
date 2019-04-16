5c32be110d17d5418322d6fd71404d47e7210b05
Raúl Kripalani <raul@protocol.ai>
2019-04-16 18:17:23 +0100

    switch to go-libp2p-core.

--- protocol.go
+++ protocol.go
@@ -1,9 +1,9 @@
 package protocol
 
-import moved "github.com/libp2p/go-libp2p/skel/protocol"
+import moved "github.com/libp2p/go-libp2p-core/protocol"
 
-// Deprecated: Use github.com/libp2p/go-libp2p/skel/protocol.ID instead.
+// Deprecated: Use github.com/libp2p/go-libp2p-core/protocol.ID instead.
 type ID = moved.ID
 
-// Deprecated: Use github.com/libp2p/go-libp2p/skel/protocol.TestingID instead.
+// Deprecated: Use github.com/libp2p/go-libp2p-core/protocol.TestingID instead.
 const TestingID = moved.TestingID
